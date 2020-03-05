5954f6c8b08baf2867500c0c6a7c3b64174ce61e
Steff Burgering <steffburgering@hotmail.com>
2020-03-05 13:31:30 +0100

    conn werkt niet

---
+++ Controller/DAL/Conn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DAL
+{
+    class Conn
+    {
+        static void Main(string[] args)
+        {
+            MongoClient dbClient = new MongoClient("");
+
+            var database = dbClient.GetDatabase("sample_training");
+            var collection = database.GetCollection<BsonDocument>("grades");
+        }
+    }
+}

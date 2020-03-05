c4e3a06eb2b192b0d4d503019e02bf94afd13164
Tom <tompinguin@gmail.com>
2020-03-05 13:36:03 +0100

    shit

---
+++ Controller/DAL/Conn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DAL
+{
+    class Conn
+    {
+        static void Main(string[] args)
+        {
+            MongoClient dbClient = new MongoClient("");
+
+            var database = dbClient.GetDatabase("sample_training");
+            var collection = database.GetCollection<BsonDocument>("grades");
+
+        }
+    }
+}
